e602c29493efb5bd14c5230d93d4cdbdb7162e58
Diego Sepúlveda <diegosepu.soto@gmail.com>
2024-07-31 12:57:11 -0400

    feat: initial commit; github action testing

---
+++ src/serverless-main.ts
@@ -0,0 +1,25 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+import serverlessExpress from '@vendia/serverless-express';
+import { Callback, Context, Handler } from 'aws-lambda';
+
+let serverlessHandler: Handler;
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.init();
+
+  const appAdapter = app.getHttpAdapter().getInstance();
+  return serverlessExpress({ app: appAdapter });
+}
+
+export const handler: Handler = async (
+  event: any,
+  context: Context,
+  callback: Callback,
+) => {
+  serverlessHandler = serverlessHandler ?? (await bootstrap());
+
+  return serverlessHandler(event, context, callback);
+};
